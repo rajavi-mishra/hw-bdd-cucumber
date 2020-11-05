2a1031508c684e76abae0475267c18e9c4faefd4
Rajavi Mishra <rajavi.mishra@berkeley.edu>
2020-11-05 07:07:38 +0000

    complete

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -24,10 +24,25 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: 'PG', 'R'
   # enter step(s) to uncheck all other checkboxes
+  When I uncheck the following ratings: 'G', 'PG-13'
   # enter step to "submit" the search form on the homepage
+  And I press the 'Refresh' button
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see the following movies: 'The Terminator',  'When Harry Met Sally', 'Amelie', 'The Incredibles', 'Raiders of the Lost Ark'  
   # enter step(s) to ensure that other movies are not visible
+  And I cannot see the following movies: 'Aladdin', 'The Help', 'Chocolat', '2001: A Space Odyssey', 'Chicken Run'
 
 Scenario: all ratings selected
   # see assignment
+  When I follow ratings
+  Then I should see "Aladdin" before "Amelie"
+  Then I should see "Amelie" before "Chicken Run"
+  Then I should see "Chicken Run" before "Chocolat"
+  Then I should see "Chocolat" before "Raiders of the Lost Ark"
+  Then I should see "Raiders of the Lost Ark" before "The Help"
+  Then I should see "The Help" before "The Incredibles"
+  Then I should see "The Incredibles" before "The Terminator"
+  Then I should see "The Terminator" before "When Harry Met Sally"
+  Then I should see "When Harry Met Sally" before "2001: A Space Odyssey"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+      Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  expression = /.*{e1}.*{e2}.*/
+  expression.match(page.all())
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +30,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  lis = rating_list.split(", ")
+  lis.each do |r|
+      if uncheck
+          uncheck "ratings_#{r}"
+      else
+          check "ratings_#{r}"
+      end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+      movie_count = page.all('table#movies tbody tr').length
+      expect(movie_count).to eq Movie.count
 end
